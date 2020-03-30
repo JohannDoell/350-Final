068c63abbf8d1faf833de78f57ef3b414fbcc893
Bronson Schultz <58102397+BronsonSchultz@users.noreply.github.com>
2020-03-30 15:44:36 -0600

    start of routing

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -6,11 +6,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 import $ from 'jquery';
 import './index.css';
-
-// === React Import ===
-
 import Header from './header.js';
 import {CategoryContainer, ThreadsContainer, Thread} from './containers.js';
 
@@ -21,17 +25,92 @@ import {CategoryContainer, ThreadsContainer, Thread} from './containers.js';
 // ==== Classes ====
 
 
+///////////// Routing practice
+
+class navBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    Home() {
+      return (
+        <div>
+          <h2>Home</h2>
+        </div>
+      );
+    }
+
+    About() {
+      return (
+        <div>
+          <h2>About</h2>
+        </div>
+      );
+    }
+
+    Dashboard() {
+      return (
+        <div>
+          <h2>Dashboard</h2>
+        </div>
+      );
+    }
+
+    render() {
+        return (
+            <div class="router">
+                <Router>
+                  <div>
+                    <ul>
+                      <li>
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                      </li>
+                    </ul>
+
+                    <hr />
+
+                    {/*
+                      A <Switch> looks through all its children <Route>
+                      elements and renders the first one whose path
+                      matches the current URL. Use a <Switch> any time
+                      you have multiple routes, but you want only one
+                      of them to render at a time
+                    */}
+                    <Switch>
+                      <Route exact path="/">
+                        {this.Home()}
+                      </Route>
+                      <Route path="/about">
+                        {this.About()}
+
+                      </Route>
+                      <Route path="/dashboard">
+                        {this.Dashboard()}
+                      </Route>
+                    </Switch>
+                  </div>
+                </Router>
+            </div>
+        );
+    }
+}
+///////////// Routing practice
 
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // Old, just to show an example.
-            showchat: true,
-            message: '',
-            chatlog: [],
-
             // New
             response: 'Not connected to server.'
 
@@ -144,6 +223,9 @@ class Main extends React.Component {
 // ========================================
 
 ReactDOM.render(
+    <Router>
+        <navBar />
+    </Router>,
     <Main />,
     document.getElementById('root')
 );
