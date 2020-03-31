8b806a2a36758c4a88ca0982399ecfa1b9381428
Johann Doell (jsd090) <jsd090@mail.usask.ca>
2020-03-30 21:13:03 -0600

    Created login ui

--- hello-world/src/header.js
+++ hello-world/src/header.js
@@ -40,6 +40,7 @@ class Header extends React.Component {
         return (
             <div className="header">
             <Banner/>
+            <Login></Login>
             <p>Welcome user!</p>
             <NavBar/>
             </div>
@@ -52,6 +53,40 @@ class Credits extends React.Component {
         return (
             <div className="credits">
                 <p>CMPT 350 - Winter 2019-2020</p>
+            </div>
+        );
+    }
+}
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <p>Login:</p>
+                <label>
+                    <input type="text" value={this.state.username} onChange={this.handleChangeUsername}></input>
+                    <input type="password" value={this.state.password} onChange={this.handleChangePassword}></input>
+                    <input type="submit" value="Submit"/>
+                </label>
             </div>
         );
     }
